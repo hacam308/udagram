9a18210afe2500ca408af3d133748e62f0c8fb95
hacam308 <hacam308@gmail.com>
2023-10-29 23:53:44 +0700

    16 push

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'udagram-api-dev.eba-mekua7fm.us-east-1.elasticbeanstalk.com ',
+  apiHost: 'udagram-api-dev.eba-mekua7fm.us-east-1.elasticbeanstalk.com',
 };
 
 /*
--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'udagram-api-dev.eba-mekua7fm.us-east-1.elasticbeanstalk.com '
+  apiHost: 'udagram-api-dev.eba-mekua7fm.us-east-1.elasticbeanstalk.com'
 };
 
 /*
